92b21be4c6e8d495d6541caaf264c0c866e00503
Ajmal112 <118426017+Ajmal112@users.noreply.github.com>
2023-02-14 14:20:59 +0530

    initial commit

---
+++ finding_divisible.js
@@ -0,0 +1,24 @@
+
+/*
+let n = 15;
+let m = 5;
+
+// Write code to print 
+if n is a multiple of m 
+or n is not a multiple of m
+
+For the sample input put it should print
+
+ 15 is a multiple of 5
+ 
+ hint: 12%3 gives 0.
+*/
+
+let n = 15;
+let m = 5;
+
+if( n%m == 0) {
+  console.log("15 is a multiple of 5");
+} else {
+    console.log("15 is not a multiple of 5");  
+}
